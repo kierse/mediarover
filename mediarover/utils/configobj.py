73a783910f0a457d3fedd94290edc94ebd373813
Kieran Elliott <kierse@pissiphany.com>
2009-11-30 11:07:35 -0700

    removed copyright notice
    removed copyright notice from the top of the ConfigObj and ConfigObj validate
    files.

--- mediarover/utils/configobj.py
+++ mediarover/utils/configobj.py
@@ -1,18 +1,3 @@
-# Copyright 2009 Kieran Elliott <kierse@mediarover.tv>
-#
-# Media Rover is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# Media Rover is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # configobj.py
 # A config file reader/writer that supports nested sections in config files.
 # Copyright (C) 2005-2008 Michael Foord, Nicola Larosa
