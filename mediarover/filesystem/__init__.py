447355366ef07c0553e7ac9b5f1ab952323c3d18
Kieran Elliott <kierse@mediarover.tv>
2010-04-08 23:37:41 -0600

    refactored filesystem episode factory method

--- mediarover/filesystem/__init__.py
+++ mediarover/filesystem/__init__.py
@@ -13,24 +13,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from mediarover.error import InvalidEpisodeString
 from mediarover.filesystem.episode.single import FilesystemSingleEpisode
 from mediarover.filesystem.episode.daily import FilesystemDailyEpisode
 from mediarover.filesystem.episode.multi import FilesystemMultiEpisode
 
-def create_filesystem_episode(series, path):
+def create_filesystem_episode(series, path, **kwargs):
 	
-	if FilesystemMultiEpisode.handle(path):
-		params = FilesystemMultiEpisode.extract_from_string(path, series=series)
-		return FilesystemMultiEpisode(**params)
-
-	elif FilesystemSingleEpisode.handle(path):
-		params = FilesystemSingleEpisode.extract_from_string(path, series=series)
-		return FilesystemSingleEpisode(**params)
-
-	elif FilesystemDailyEpisode.handle(path):
-		params = FilesystemDailyEpisode.extract_from_string(path, series=series)
-		return FilesystemDailyEpisode(**params)
+	episode = None
+	if 'episode' in kwargs:
+		episode = kwargs['episode']
+		try:
+			episode.episodes
+		except AttributeError:
+			try:
+				episode.year
+			except AttributeError:
+				episode = FilesystemSingleEpisode.new_from_episode(episode, path)
+			else:
+				episode = FilesystemDailyEpisode.new_from_episode(episode, path)
+		else:
+			episode = FilesystemMultiEpisode.new_from_episode(episode, path)
 
 	else:
-		raise ""
+		if FilesystemMultiEpisode.handle(path):
+			params = FilesystemMultiEpisode.extract_from_string(path, series=series, **kwargs)
+			episode = FilesystemMultiEpisode(**params)
+
+		elif FilesystemSingleEpisode.handle(path):
+			params = FilesystemSingleEpisode.extract_from_string(path, series=series, **kwargs)
+			episode = FilesystemSingleEpisode(**params)
+
+		elif FilesystemDailyEpisode.handle(path):
+			params = FilesystemDailyEpisode.extract_from_string(path, series=series, **kwargs)
+			episode = FilesystemDailyEpisode(**params)
+
+		else:
+			raise InvalidEpisodeString("unable to identify episode type: %r" % path)
+
+	return episode
 
