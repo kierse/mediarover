1570c5940bb03badbd25d898490a82c018084788
Kieran Elliott <kierse@mediarover.tv>
2010-04-22 19:43:33 -0600

    adding sabnzbd queue episode and factory objects

---
+++ mediarover/queue/sabnzbd/factory.py
@@ -0,0 +1,82 @@
+# Copyright 2009 Kieran Elliott <kierse@mediarover.tv>
+#
+# Media Rover is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Media Rover is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from mediarover.config import ConfigObj
+from mediarover.episode.single import SingleEpisode
+from mediarover.episode.daily import DailyEpisode
+from mediarover.episode.multi import MultiEpisode
+from mediarover.factory import EpisodeFactory
+from mediarover.series import Series
+from mediarover.source.newzbin.episode import NewzbinSingleEpisode, NewzbinDailyEpisode, NewzbinMultiEpisode
+from mediarover.utils.injection import is_instance_of, Dependency
+
+class SabnzbdFactory(EpisodeFactory):
+
+	# class variables- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	# declare module dependencies
+	config = Dependency("config", is_instance_of(ConfigObj))
+	watched_series = Dependency("watched_series", is_instance_of(dict))
+
+	# public methods - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	def create_episode(self, string, **kwargs):
+
+		newzbin_job = kwargs['newzbin']
+		del kwargs['newzbin']
+
+		params = None
+		if newzbin_job:
+			if NewzbinMultiEpisode.handle(string):
+				params = NewzbinMultiEpisode.extract_from_string(string, **kwargs)
+			elif NewzbinSingleEpisode.handle(string):
+				params = NewzbinSingleEpisode.extract_from_string(string, **kwargs)
+			elif NewzbinDailyEpisode.handle(string):
+				params = NewzbinDailyEpisode.extract_from_string(string, **kwargs)
+			else:
+				raise InvalidEpisodeString("unable to identify episode type: %r" % string)
+		else:
+			if MultiEpisode.handle(string):
+				params = MultiEpisode.extract_from_string(string, **kwargs)
+			elif SingleEpisode.handle(string):
+				params = SingleEpisode.extract_from_string(string, **kwargs)
+			elif DailyEpisode.handle(string):
+				params = DailyEpisode.extract_from_string(string, **kwargs)
+			else:
+				raise InvalidEpisodeString("unable to identify episode type: %r" % string)
+
+		# add job id to list of parameters
+		params['id'] = job_id
+
+		# locate series object.  If series is unknown, create new series
+		sanitized_series = Series.sanitize_series_name(name=params['series'])
+		if sanitized_series in self.watched_series:
+			params['series'] = self.watched_series[sanitized_series]
+		else:
+			params['series'] = Series(params['series'])
+
+		if 'quality' not in kwargs:
+			if sanitized_series in self.config['tv']['filter']:
+				params['quality'] = self.config['tv']['filter'][sanitized_series]['quality']['desired']
+			else:
+				params['quality'] = self.config['tv']['quality']['desired']
+
+		if 'start_episode' in params:
+			return SabnzbdMultiEpisode(**params)
+		elif 'year' in params:
+			return SabnzbdDailyEpisode(**params)
+		else:
+			return SabnzbdSingleEpisode(**params)
+
