61c3faec02c961833726d14c7bdd98dc56e38bd8
Kieran Elliott <kierse@mediarover.tv>
2010-10-03 12:42:43 -0600

    updated single episode regex
    updated single episode regex to account for a possible space or '.' between
    the season and episode numbers (ie. s04 e13 or s04.e13)

--- mediarover/episode/single.py
+++ mediarover/episode/single.py
@@ -27,7 +27,7 @@ class SingleEpisode(Episode):
 
 	__supported_patterns = (
 		# episode 1 regex, ie. s03e10
-		re.compile("s(?P<season>\d{1,2})e(?P<episode>\d{1,3})", re.IGNORECASE),
+		re.compile("s(?P<season>\d{1,2})(?:\.|\s)?e(?P<episode>\d{1,3})", re.IGNORECASE),
 
 		# episode 2 regex, ie. 3x10
 		re.compile("(?P<season>\d{1,2})x(?P<episode>\d{1,3})", re.IGNORECASE)
